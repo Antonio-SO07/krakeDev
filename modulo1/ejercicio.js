7a2ed54891058ce921d1e2eeb66dea8c015f4526
Antonio-SO07 <alejoumaquinga67@gmail.com>
2025-09-25 21:29:40 -0500

    el boton modificar

--- modulo1/ejercicio.js
+++ modulo1/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let imagenSaludo;
+    imagenSaludo=document.getElementById("imgSaludo");
+    imagenSaludo.src="./imagenes/ganar2.gif"
     console.log("ingreso a saludar")
+
 }
