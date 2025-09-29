2a4bbaaa08fb0c1d512554234a274d338f6b673a
Antonio-SO07 <alejoumaquinga67@gmail.com>
2025-09-29 11:23:41 -0500

    funcion recuperarTexto ok

---
+++ utilitarios/saludo.js
@@ -0,0 +1,14 @@
+saludar=function(){
+    //recuperar el valor de la caja de texto txt nombre
+    let nombre=recuperarTexto("txtNombre");
+    //recuperar el valor de la caja de texto txt apellido
+    let apellido=recuperarTexto("txtApellido");
+}
+
+recuperarTexto=function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.value;
+    return valorIngresado;
+}
