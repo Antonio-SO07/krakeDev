0d2673b7269d9b2369fcb9a8688f7434620a0d00
Antonio-SO07 <alejoumaquinga67@gmail.com>
2025-09-30 10:02:20 -0500

    promedio notas funcionando correctamente

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,9 @@
+calcularPromedioNotas=function(){
+    let nota1=recuperarFloat("txt1");
+    let nota2=recuperarFloat("txt2");
+    let nota3=recuperarFloat("txt3");
+    let promedio=calcularPromedio(nota1,nota2,nota3);
+    let cmpPromedio=promedio.toFixed(2);
+    mostrarTexto("lblResultado",cmpPromedio);
+    
+}
