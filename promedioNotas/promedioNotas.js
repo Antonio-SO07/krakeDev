90f40888ceb9310da27d7b99e6550dbe1e86ebd3
Antonio-SO07 <alejoumaquinga67@gmail.com>
2025-10-06 13:29:59 -0500

    cambios promedios notas ok

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,9 +5,17 @@ calcularPromedioNotas=function(){
     let promedio=calcularPromedio(nota1,nota2,nota3);
     let cmpPromedio=promedio.toFixed(2);
     mostrarTexto("lblResultado",cmpPromedio);
-    if(cmpPromedio>7){
-        mostrarImagen("lblImagen","./imagenes/gilcat.gif")
+    if(cmpPromedio<5 && cmpPromedio>=0){
+        mostrarTexto("lblMensaje","REPROBADO");
+        mostrarImagen("lblImagen","./imagenes/fracaso.gif");
+    }else if(cmpPromedio>=5 && cmpPromedio<=8){
+        mostrarTexto("lblMensaje","BUEN TRABAJO");
+        mostrarImagen("lblImagen","./imagenes/400.gif");
+    }else if(cmpPromedio>8 && cmpPromedio<=10){
+        mostrarTexto("lblMensaje","EXCELENTE");
+        mostrarImagen("lblImagen","./imagenes/gilcat.gif");
     }else{
-        mostrarImagen("lblImagen","./imagenes/fracaso.gif")
+        mostrarTexto("lblMensaje","DATOS INCORRECTOS");
+        mostrarImagen("lblImagen","./imagenes/error.gif");
     }
 }
