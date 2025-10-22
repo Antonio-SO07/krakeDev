0960973551645d56ab4e55dfd269c63d5377d455
Antonio-SO07 <alejoumaquinga67@gmail.com>
2025-10-21 22:31:19 -0500

    invertir cadena listogit status

---
+++ bucles/ejerciciosCadenas.js
@@ -0,0 +1,41 @@
+ejecutarPrueba = function () {
+    let mensaje;
+    mensaje=recuperarTexto("txtCadena");
+    recorrerCadena(mensaje);
+}
+
+ejecutarPrueba2 = function() {
+    let mensaje;
+    mensaje = recuperarTexto("txtCadena");
+    invertirCadena(mensaje);
+    
+}
+
+recorrerCadena = function (cadena) {
+    //0123
+    //juan
+    let caracter;
+
+    for (let posicion = 0; posicion < cadena.length; posicion++) {
+        caracter = cadena.charAt(posicion);
+        console.log("Carcater " + caracter + " posicion " + posicion);
+    }
+    for (let posicion = 0; posicion <= cadena.length - 1; posicion++) {
+        caracter = cadena.charAt(posicion);
+        console.log("CARACTER " + caracter + " posicion " + posicion);
+    }
+
+}
+
+
+
+invertirCadena = function(cadena) {
+    let resultado="";
+    for (let posicion = cadena.length -1; posicion >= 0; posicion--) {
+        caracter = cadena.charAt(posicion);
+        console.log("Caracter " + caracter + " posicion " + posicion);
+        resultado=resultado+caracter;
+         
+    }
+    mostrarTexto("lblResultado",resultado);
+}
