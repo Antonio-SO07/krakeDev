d43bc097747c513c2514a76b1c3d3665b185fdfd
Antonio-SO07 <alejoumaquinga67@gmail.com>
2025-10-22 15:19:36 -0500

    ahorcado listo

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,5 +1,8 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
-let palabraSecreta;
+let palabraSecreta = "" ;
+let intentos = 0;
+let coincidencias = 0;
+let errores = 0;
 
 esMayuscula = function (caracter) {
     let cmpCarac;
@@ -29,22 +32,22 @@ guardarPalabra = function () {
 
 
 mostrarLetra = function (letra, posicion) {
-    
-    if(posicion == 0){
+
+    if (posicion == 0) {
         console.log(letra)
-        mostrarTexto("div0",letra);
+        mostrarTexto("div0", letra);
     }
-    if(posicion == 1){
-        mostrarTexto("div1",letra);
+    if (posicion == 1) {
+        mostrarTexto("div1", letra);
     }
-    if(posicion == 2){
-        mostrarTexto("div2",letra);
+    if (posicion == 2) {
+        mostrarTexto("div2", letra);
     }
-    if(posicion == 3){
-        mostrarTexto("div3",letra);
+    if (posicion == 3) {
+        mostrarTexto("div3", letra);
     }
-    if(posicion == 4){
-        mostrarTexto("div4",letra);
+    if (posicion == 4) {
+        mostrarTexto("div4", letra);
     }
 
 }
@@ -55,18 +58,61 @@ validar = function (letra) {
     for (let i = 0; i < palabraSecreta.length; i++) {
         if (palabraSecreta[i] == letra) {
             mostrarLetra(letra, i);
-            letrasEncontradas++;
+            letrasEncontradas++;                  
         }
+    }
+    if (letrasEncontradas > 0) {
+        coincidencias+=letrasEncontradas
+    }else{
+        errores++;
+        mostrarAhorcado();
+    }
+
+
+
+}
+
+
+ingresarLetra = function () {
+    intentos++;
+    console.log("INTENTO N°" + intentos);
+    let letra = recuperarTexto("txtLetra");
+    if (esMayuscula(letra)) {
+        validar(letra);
+        if (coincidencias == 5) {
+            mostrarImagen("ahorcadoImagen", "ganador.gif");
+        }
+        if (intentos == 10) {
+            mostrarImagen("ahorcadoImagen", "gameOver.gif")
+            palabraSecreta="";
+            document.getElementById("txtSecreta")
+        }
+        mostrarTextoEnCaja("txtLetra", "");
+    } else {
+        alert("SOLO SE ACEPTAN MAYUSCULAS");
     }
 
 }
 
 
-ingresarLetra=function(){
-    let letra=recuperarTexto("txtLetra");
-    if(esMayuscula(letra)){
-        validar(letra);
-    } else {
-        alert("SOLO SE ACEPTAN MAYUSCULAS");
+mostrarAhorcado = function () {
+    if (errores == 1) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_01.png");
+    } else if (errores == 2) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_02.png");
+    } else if (errores == 3) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_03.png");
+    } else if (errores == 4) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_04.png");
+    } else if (errores == 5) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_05.png");
+    } else if (errores == 6) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_06.png");
+    } else if (errores == 7) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_07.png");
+    } else if (errores == 8) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_08.png");
+    } else if (errores == 9) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_09.png");
     }
 }
