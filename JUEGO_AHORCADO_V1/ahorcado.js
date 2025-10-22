96e90f20d9fdbafb287e74ba0a9b5ba1a00ae65c
Antonio-SO07 <alejoumaquinga67@gmail.com>
2025-10-22 09:53:36 -0500

    paso 1 completo

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,4 +1,10 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta = "";
+
+
+
+
+
 esMayuscula = function (caracter) {
     let cmpCarac;
     cmpCarac = caracter.charCodeAt(0);
@@ -6,5 +12,21 @@ esMayuscula = function (caracter) {
         return true;
     } else {
         return false;
+    }
+}
+
+guardarPalabra = function () {
+    let palabra = recuperarTexto("txtSecreta");
+    console.log(palabra);
+    if (palabra.length !== 5) {
+        alert("Debe ingresar una palabra de 5 letras mayúsculas");
+        return;
+    }
+
+    for (let i = 0; i < palabra.length; i++) {
+        if (!esMayuscula(palabra[i])) {
+            alert("Debe ingresar una palabra de 5 letras mayúsculas");
+            return;
+        }
     }
 }
