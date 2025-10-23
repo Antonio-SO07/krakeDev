b28037b77ac56117ea76d8283e3063e67cd6b434
Antonio-SO07 <alejoumaquinga67@gmail.com>
2025-10-23 17:45:13 -0500

    ROL DE PAGOS PARTE 2 LISTA

--- roles/roles.js
+++ roles/roles.js
@@ -1,25 +1,53 @@
 let empleados = [
-    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
+    { cedula: "1750816322", nombre: "Alejo", apellido: "Salcedo", sueldo: 800.0 }
 ]
 
-mostrarOpcionEmpleado=function(){
-    
+mostrarOpcionEmpleado = function () {
+
     mostrarComponente('divEmpleado');
     ocultarComponente('divRol');
     ocultarComponente('divResumen');
+    mostrarEmpleados();
 }
 
-mostrarOpcionRol=function(){
-    
+mostrarOpcionRol = function () {
+
     mostrarComponente('divRol');
     ocultarComponente('divEmpleado');
     ocultarComponente('divResumen');
 }
 
-mostrarOpcionResumen=function(){
-    
+mostrarOpcionResumen = function () {
+
     mostrarComponente('divResumen');
     ocultarComponente('divRol');
     ocultarComponente('divEmpleado');
+}
+
+
+mostrarEmpleados = function () {
+    let cmpTabla = document.getElementById("tablaEmpleados");
+
+    let contenidoTabla = "<table>" +
+        "<tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>APELLIDO</th>" +
+        "<th>SUELDO</th>" +
+        "</tr>";
+
+    for (let i = 0; i < empleados.length; i++) {
+        let emp = empleados[i];
+        contenidoTabla += "<tr>" +
+            "<td>" + emp.cedula + "</td>" +
+            "<td>" + emp.nombre + "</td>" +
+            "<td>" + emp.apellido + "</td>" +
+            "<td>" + emp.sueldo + "</td>" +
+            "</tr>";
+    }
+
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
 }
